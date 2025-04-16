d51f27011b8fc53dd917ee40b42038f6d781f183
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2025-04-15 23:37:20 -0300

    study: type annotation

---
+++ Typescript/src/type-annotation.ts
@@ -0,0 +1,13 @@
+const text: string = "hi";
+const age: number = 24;
+const isVisible: boolean = true;
+const key: symbol = Symbol("key");
+
+const numbers: Array<number> = [1, 2, 3];
+const strings: string[] = ["a", "b"];
+
+const person: { name: string; age?: number } = {
+  name: "Alex",
+};
+
+const sum = (x: number, y: number): number => x + y;
