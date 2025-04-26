b79185575b3f5b313e08e41d2007d7dd723e05e0
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2025-04-25 22:45:00 -0300

    study: class

---
+++ Typescript/src/14.class.ts
@@ -0,0 +1,23 @@
+class Company {
+  public readonly name: string;
+  private readonly employee: Employee[] = [];
+  protected readonly cnpj: string;
+
+  constructor(name: string, cnpj: string) {
+    this.name = name;
+    this.cnpj = cnpj;
+  }
+
+  addEmployee(employ: Employee) {
+    this.employee.push(employ);
+  }
+}
+
+class Employee {
+  constructor(public readonly name: string, public readonly lastName: string) {}
+}
+
+const employ1 = new Employee("Joao", "Silva");
+
+const company1 = new Company("Apple", "00.000.000/0001-00");
+console.log(company1.name);
