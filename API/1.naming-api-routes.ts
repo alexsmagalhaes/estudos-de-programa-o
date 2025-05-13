45f00c28bcd55d0bbe7799102eda76af45629d57
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2025-05-13 13:10:48 -0300

    study: api routes naming

---
+++ API/1.naming-api-routes.ts
@@ -0,0 +1,19 @@
+//📦 POST   /adicionarProduto     →   /produtos
+//🔹 Regra: Use substantivos no plural
+
+//🔍 GET    /obterProduto/123     →   /produtos/123
+//🔹 Regra: A ação vem do verbo HTTP (GET, POST, PUT, DELETE)
+
+//🔡 GET    /PedidosPendentes     →   /pedidos-pendentes
+//🔹 Regra: Use letras minúsculas e kebab-case para palavras compostas
+
+//🚫 GET    /produtos/            →   /produtos
+//🔹 Regra: Não termine com barra
+
+/*
+🔹 Use substantivos no plural
+🔹 A ação vem do verbo HTTP (GET, POST, PUT, DELETE)
+🔹 Sempre em letras minúsculas
+🔹 Palavras compostas? Use kebab-case!
+🔹 Evite barras extras no final
+*/
