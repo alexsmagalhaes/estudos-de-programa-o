fd1b0b9a9e59187e874a93c79ddc135b09014aef
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2025-05-16 18:35:37 -0300

    study: atividades java

--- Java POO/object-orientation/src/main/main.java
+++ Java POO/object-orientation/src/main/main.java
@@ -1,23 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main;
 
+import main.atividade2.Dice;
 
-/**
- *
- * @author Aluno
- */
 public class main {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        
 
+        Dice x = new Dice();
+
+        System.out.println(x.roll());
     }
-    
+
 }
